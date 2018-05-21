e4a91ee7e3f7a542391bbc1a0cabcd7072d6d8fc
Oliver Drozdowski <drozdowo@uwindsor.ca>
2018-05-21 19:50:39 -0400

    Set up css-loader, style-loader, made a test React component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+import './index.css';
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
+
 class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {toggle: true};
+
+        this.doTest = this.doTest.bind(this);
+    }
+    doTest(){
+        this.setState(prevState => ({
+            toggle: !prevState.toggle
+        }));
+    }
     render(){
-        return <h1> Welcome to Eve Online Fitting Manager! </h1>
+        return (
+            <div className='mainApp'>
+                <h1> Welcome to Eve Online Fitting Manager! </h1>
+                <button onClick={this.doTest}>
+                 Content Here {this.state.toggle ? 'ON' : 'OFF'}
+                </button>
+            </div>
+        );
     }
 }
 
-const theApp = <App />
-
-ReactDOM.render(theApp, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
